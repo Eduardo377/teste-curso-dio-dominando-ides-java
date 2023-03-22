b842f97eaf789857455709e026e030ce9e93551a
Eduardo377 <eduardogomes377@gmail.com>
2023-03-21 21:32:31 -0300

    fix: aperfeicoamento da calculadora

--- src/br/com/dio/calculadora/Calculdadora.java
+++ src/br/com/dio/calculadora/Calculdadora.java
@@ -1,5 +1,6 @@
 package br.com.dio.calculadora;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Calculdadora {
@@ -7,37 +8,36 @@ public class Calculdadora {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
-		int a, b;
+		double a, b;
 
 		System.out.println("Digite o primeiro valor");
 		a = scan.nextInt();
 		System.out.println("Digite o segundo valor");
 		b = scan.nextInt();
+		double soma = soma(a, b);
+		double subtracao = subtracao(a, b);
+		double multiplicacao = multiplicacao(a, b);
+		double divisao = divisao(a, b);
 
-		int soma = soma(a, b);
-		int subtracao = subtracao(a, b);
-		int divisao = divisao(a, b);
-		int multiplicacao = multiplicacao(a,b);
-		
-		System.out.println("Resultado da soma: " + soma);
-		System.out.println("Resultado da subtração: " + subtracao);
-		System.out.println("Resultado da divisao: " + divisao);
-		System.out.println("Resultado da multiplicacao: " + multiplicacao);
+		System.out.println("Resultado da soma: " + NumberFormat.getCurrencyInstance().format(soma));
+		System.out.println("Resultado da subtração: " + NumberFormat.getCurrencyInstance().format(subtracao));
+		System.out.println("Resultado da divisao: " + NumberFormat.getCurrencyInstance().format(divisao));
+		System.out.println("Resultado da multiplicacao: " + NumberFormat.getCurrencyInstance().format(multiplicacao));
 	}
 
-	public static int soma(int a, int b) {
+	public static double soma(double a, double b) {
 		return a + b;
 	}
 
-	public static int subtracao(int a, int b) {
+	public static double subtracao(double a, double b) {
 		return a - b;
 	}
 
-	public static int multiplicacao(int a, int b) {
+	public static double multiplicacao(double a, double b) {
 		return a * b;
 	}
 
-	public static int divisao(int a, int b) {
+	public static double divisao(double a, double b) {
 		return a / b;
 	}
 
